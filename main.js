11174d536986664d991a8b6debf4a95f36c5a723
UminN-738 <yuminn1202@gmail.com>
2024-11-24 17:52:37 -0800

    ver2

---
+++ main.js
@@ -0,0 +1,5 @@
+// Add event listener for the Pokemon title
+document.querySelector(".sidebar h1").addEventListener("click", function () {
+    // Redirect to the front page (index.html)
+    window.location.href = "home.html"; // Replace with your front page file
+});
